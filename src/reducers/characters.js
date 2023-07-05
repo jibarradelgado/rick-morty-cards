ac67b81826d355775d69a8429012f0fd927c4131
Jorge Ibarra <hello@jorgeibarra.com.mx>
2023-07-05 15:00:02 -0600

    Added immutable to improve the immutability of the app structure

--- src/reducers/characters.js
+++ src/reducers/characters.js
@@ -1,28 +1,28 @@
+import { fromJS } from "immutable"
 import { SET_CHARACTERS, SET_FAVORITE, SET_LOADING } from "../actions/types"
 
-const initialState = {
+const initialState = fromJS({
   characters: [],
   loading: false,
-}
+})
 
 export const charactersReducer = (state = initialState, action) => {
   switch(action.type) {
     case SET_CHARACTERS: 
-      return { ...state, characters: action.payload}
+      return state.setIn(['characters'], fromJS(action.payload))
     case SET_FAVORITE: 
-      const newCharactersList = [...state.characters]
-      const currentCharacterIndex = newCharactersList.findIndex((character) => {
-        return character.id === action.payload.characterId
+      const currentCharacterIndex = state.get('characters').findIndex((character) => {
+        return character.get('id') === action.payload.characterId
       })
 
       if (currentCharacterIndex < 0) {
         return state
       }
-      
-      newCharactersList[currentCharacterIndex].favorite = !newCharactersList[currentCharacterIndex].favorite
-      return { ...state, characters: newCharactersList }
+      const isFavorite = state.getIn(['characters', currentCharacterIndex, 'favorite'])
+
+      return state.setIn(['characters', currentCharacterIndex, 'favorite'], !isFavorite)
     case SET_LOADING: 
-      return {...state, loading: action.payload}
+      return state.setIn(['loading'], action.payload)
     default:
       return state
   }
